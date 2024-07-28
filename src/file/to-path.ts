526c6e44fa3ae2856ff212cd51baaf13a84a4bdb
mancuoj <mancuoj@gmail.com>
2024-07-28 21:50:31 +0800

    docs(file): add comment

--- src/file/to-path.ts
+++ src/file/to-path.ts
@@ -1,5 +1,18 @@
 import { fileURLToPath } from 'node:url'
 
+/**
+ * Converts a URL object or a string path to a string path.
+ *
+ * @param {URL | string} path - The URL object or string path to convert.
+ * @returns {string} The converted string path.
+ *
+ * @example
+ * const url = new URL('file:///tmp/hello');
+ * console.log(toPath(url)); //=> '/tmp/hello'
+ *
+ * const path = '/tmp/hello';
+ * console.log(toPath(path)); //=> '/tmp/hello'
+ */
 export function toPath(path: URL | string): string {
   return path instanceof URL ? fileURLToPath(path) : path
 }
