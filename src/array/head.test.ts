94154f4c5b925eea26893d58bf8847e1ca703b86
mancuoj <mancuoj@gmail.com>
2024-07-29 01:07:11 +0800

    feat(array): get the first element of an array

---
+++ src/array/head.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from 'vitest'
+import { head } from './head'
+
+describe('head', () => {
+  it('returns the first element of an array or undefined if the array is empty', () => {
+    expect(head([1, 2, 3])).toBe(1)
+    expect(head(['a', 'b', 'c'])).toBe('a')
+    expect(head([true, false, true])).toBe(true)
+    expect(head([])).toBeUndefined()
+  })
+})
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from './array'
 export * from './ds'
 export * from './file'
 export * from './object'
