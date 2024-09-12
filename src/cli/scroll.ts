95bbb6f3b10f4e1170ec48918bd889070dde4220
mancuoj <mancuoj@gmail.com>
2024-09-12 23:45:40 +0800

    docs: update

--- src/cli/scroll.ts
+++ src/cli/scroll.ts
@@ -1,6 +1,13 @@
 import { CSI } from './cursor'
 
 export const scroll = {
+  /**
+   * Scroll display up a specific amount of lines. Default to 1.
+   */
   up: (count = 1) => `${CSI}S`.repeat(count),
+
+  /**
+   * Scroll display down a specific amount of lines. Default to 1.
+   */
   down: (count = 1) => `${CSI}T`.repeat(count),
 }
